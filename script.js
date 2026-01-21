441e8e5ba63320fcae88d9ad0358b6aa04a08f93
Higo Viganor Camporez <87453562+hvcamporez@users.noreply.github.com>
2026-01-21 15:05:35 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,27 +1,14 @@
-const readline = require("readline");
+// Entrada de dados
+const valor = parseFloat(prompt("Informe o valor:"));
+const porcentagem = parseFloat(prompt("Informe a porcentagem:"));
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
+// Cálculo do valor da porcentagem
+const valorPorcentagem = (porcentagem * valor) / 100;
 
-rl.question("Informe o valor: ", (valorInput) => {
-  const valor = parseFloat(valorInput);
+// Cálculo do desconto e dos juros
+const valorDesconto = valor - valorPorcentagem;
+const valorJuros = valor + valorPorcentagem;
 
-  rl.question("Informe a porcentagem: ", (porcentagemInput) => {
-    const porcentagem = parseFloat(porcentagemInput);
-
-    // Cálculo do valor da porcentagem
-    const valorPorcentagem = (porcentagem * valor) / 100;
-
-    // Cálculo do desconto e dos juros
-    const valorDesconto = valor - valorPorcentagem;
-    const valorJuros = valor + valorPorcentagem;
-
-    // Exibição dos resultados
-    console.log(`\n${valor} com ${porcentagem}% de desconto é ${valorDesconto}`);
-    console.log(`${valor} com ${porcentagem}% de juros é ${valorJuros}`);
-
-    rl.close();
-  });
-});
+// Saída dos resultados
+document.write(`${valor} com ${porcentagem}% de desconto é ${valorDesconto}<br>`);
+document.write(`${valor} com ${porcentagem}% de juros é ${valorJuros}`);
